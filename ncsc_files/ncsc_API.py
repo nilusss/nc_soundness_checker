dd65068ccfc576ab1a1c7bce5fb020d30b129781
nilusss <48320346+nilusss@users.noreply.github.com>
2020-01-17 14:52:22 +0100

    Added more functions

--- ncsc_files/ncsc_API.py
+++ ncsc_files/ncsc_API.py
@@ -4,10 +4,25 @@ import maya.cmds as mc
 
 
 def ncsc_select_all_objs():
-    return mc.menuItem("ncsc_select_all_geo", q=True, checkbox=True)
+    """Return all objects in the scene
+
+    Returns:
+        list -- Return all objects using a checkbox from the UI
+    """
+
+    if mc.window("nc_soundness_checker", exists=True):
+        return mc.menuItem("ncsc_select_all_geo", q=True, checkBox=True)
 
 
 def ncsc_get_mesh(obj):
+    """Return the object if it is of type 'mesh'
+
+    Arguments:
+        obj {string} -- Object to check if it is of type 'mesh'
+
+    Returns:
+        string -- Final object that is of type 'mesh'
+    """
 
     obj_type = mc.objectType(obj)
 
@@ -19,12 +34,24 @@ def ncsc_get_mesh(obj):
 
 
 def ncsc_get_meshes_from_sel():
+    """Get a list of all selected objects in the viewport
+
+    Returns:
+        list -- All objects from the viewport that are of type 'mesh'
+    """
+
     sel = mc.ls(sl=True)
     meshes = [ncsc_get_mesh(obj) for obj in sel]
     return meshes
 
 
 def ncsc_get_obj_from_pref():
+    """Select objects based on preference
+
+    Returns:
+        list -- Objects based on preference
+    """
+
     sel = []
 
     if ncsc_select_all_objs():
--- ncsc_files/ncsc_geo.py
+++ ncsc_files/ncsc_geo.py
@@ -5,6 +5,9 @@ import ncsc_API as api
 
 
 def ncsc_delete_history():
+    """Delete the history on all or selected objects
+    """
+
     sel = api.ncsc_get_meshes_from_sel()
 
     for obj in sel:
@@ -18,16 +21,68 @@ def ncsc_delete_history():
 
 
 def ncsc_get_borders():
+    """Select borders on all or selected objects
+    """
+
     if api.ncsc_select_all_objs():
         mc.select(mc.ls(exactType="mesh"))
+
     mc.polySelectConstraint(m=3, t=0x8000, w=1)
     mc.polySelectConstraint(dis=True)
 
 
 def ncsc_get_borders_fix():
+    """A fix solution for the open borders
+    """
+
     sel = mc.ls(sl=True)
 
     for obj in sel:
         mc.polyCloseBorder(obj, ch=1)
 
     mc.select(clear=True)
+
+
+def ncsc_more_than_four():
+    """Checks for polys with more that four sides
+    """
+
+    api.ncsc_get_obj_from_pref()
+
+    mel.eval('polyCleanupArgList 4 {"0", "2", "1", "0", "1", "0", "0", "0", "0", "1e-05", "0", "1e-05", "0", "1e-05", "0", "1", "0", "0"}')
+
+
+def ncsc_more_than_four_fix():
+    """Fix polys with more than four sides
+    """
+
+    api.ncsc_get_obj_from_pref()
+
+    mel.eval('polyCleanupArgList 4 {"0", "1", "1", "0", "1", "0", "0", "0", "0", "1e-05", "0", "1e-05", "0", "1e-05", "0", "1", "0", "0"}')
+
+
+def ncsc_normal_dir():
+    pass
+
+
+def ncsc_normal_dir_fix():
+    pass
+
+
+def ncsc_non_manifold_faces():
+    """Check for non manifold geometry and select it
+    """
+
+    api.ncsc_get_obj_from_pref()
+
+    mel.eval('polyCleanupArgList 3 {"0", "2","1","0","0","0","0","0","0","1e-05","0","1e-05","0","1e-05","0","2","0"}')
+
+
+def ncsc_non_manifold_faces_fix():
+    """Check for non manifold geometry and select it
+    """
+
+    api.ncsc_get_obj_from_pref()
+
+    mel.eval('polyCleanupArgList 3 {"0", "1","1","0","0","0","0","0","0","1e-05","0","1e-05","0","1e-05","0","2","0"}')
+
